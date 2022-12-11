204a5e14ee8470a2166495f6476156f171a2d5c8
Sophie Chiang <sophieyoshi2014@gmail.com>
2022-12-11 10:08:24 +0000

    day11

--- AoC 2022/day11.py
+++ AoC 2022/day11.py
@@ -9,7 +9,7 @@ monkeys = {
     7: [73, 59, 82, 65]
 }
 
-moves = {0: lambda x:x*13, 1: lambda x:x+2, 2:lambda x:x+8, 3:lambda x:x+1, 4: lambda x:x*17, 5: lambda x:x+3, 6: lambda x:x*x, 7: lambda x:x+6}
+moves = {0: lambda x:x*13, 1: lambda x:x+2, 2: lambda x:x+8, 3: lambda x:x+1, 4: lambda x:x*17, 5: lambda x:x+3, 6: lambda x:x*x, 7: lambda x:x+6}
 rules = {0: (3,2,1), 1: (13,7,2), 2:(19,4,7), 3:(17,6,5), 4:(5,6,3), 5:(7,1,0), 6:(11,5,0), 7:(2,4,3)}
 inspect, prod = {i:0 for i in range(len(monkeys))}, 2*3*5*7*11*13*17*19
 
