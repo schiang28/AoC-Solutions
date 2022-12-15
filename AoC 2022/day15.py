3ea82679a0138977f8345093a5f24762022ab8c2
Sophie Chiang <sophieyoshi2014@gmail.com>
2022-12-15 13:52:55 +0000

    day15

--- AoC 2022/day15.py
+++ AoC 2022/day15.py
@@ -3,23 +3,30 @@ import re
 with open("day15input.txt") as f:
     file = [re.findall(r"x=(-?\d+), y=(-?\d+)", i) for i in f.read().splitlines()]
 S, NB = {tuple(map(int, i[0])): tuple(map(int, i[1])) for i in file}, set()
-level, maxxy = 2000000, 4000000
+distance = {s: abs(s[0] - b[0]) + abs(s[1] - b[1]) for s, b in S.items()}
+level, maxxy, c1, c2, bx, by = 2000000, 4000000, set(), set(), 0, 0
 
 for s,b in S.items():
     d = abs(s[0] - b[0]) + abs(s[1] - b[1])
-    length, inc = 0, True
-    if s[1]-d <= level <= s[1]+d+1:
+    if s[1] - d <= level <= s[1] + d + 1:
         for i in range(s[1] - d, s[1] + d + 1):
             if i == level:
+                length = d - (abs(i - s[1]))
                 for j in range(s[0] - length, s[0] + length + 1):
                     coord = (j, i)
                     if coord not in S.keys() and coord not in S.values():
                         NB.add(coord)
-            if length >= d:
-                inc = False
-            if inc:
-                length += 1
-            else:
-                length -= 1
+
+for s, d in distance.items():
+    c1.update([s[1] - s[0] + d + 1, s[1] - s[0] - d - 1])
+    c2.update([s[0] + s[1] + d + 1, s[0] + s[1] - d - 1])
+
+for i in c1:
+    for j in c2:
+        intersect = ((j - i) // 2, (i + j) // 2)
+        if 0 < intersect[0] < maxxy and 0 < intersect[1] < maxxy:
+            if all(abs(intersect[0]-k[0]) + abs(intersect[1]-k[1]) > distance[k] for k in S.keys()):
+                bx, by = intersect[0], intersect[1]
 
 print(len(NB))
+print(bx * maxxy + by)
---
+++ AoC 2022/test.py
@@ -0,0 +1,3 @@
+x = set([4,6])
+x.update([2,3])
+print(x)
