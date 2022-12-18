0357ad63952717d8e794d937be15acb8ffd41efe
Sophie Chiang <sophieyoshi2014@gmail.com>
2022-12-18 08:58:14 +0000

    day18

--- AoC 2022/day18.py
+++ AoC 2022/day18.py
@@ -2,7 +2,7 @@ import networkx as nx
 
 with open("day18input.txt") as f:
     file = [tuple(map(int, i.split(","))) for i in f.read().splitlines()]
-area, G = 0, nx.grid_graph(dim=[range(-1, max([i[0] for i in file])+2)] * 3)
+area, G = 0, nx.grid_graph(dim = [range(-1, max([i[0] for i in file]) + 2)] * 3)
 outside = G.copy()
 outside.remove_nodes_from(file)
 steam = nx.node_connected_component(outside, (0, 0, 0))
@@ -12,9 +12,9 @@ for i in range(len(file)):
     for j in range(len(file)):
         if i != j:
             c1, c2 = file[i], file[j]
-            if ((abs(c1[0]-c2[0]) == 1 and c1[1] == c2[1] and c1[2] == c2[2])
-            or (abs(c1[1]-c2[1]) == 1 and c1[0] == c2[0] and c1[2] == c2[2])
-            or (abs(c1[2]-c2[2]) == 1 and c1[0] == c2[0] and c1[1] == c2[1])):
+            if ((abs(c1[0] - c2[0]) == 1 and c1[1] == c2[1] and c1[2] == c2[2])
+            or (abs(c1[1] - c2[1]) == 1 and c1[0] == c2[0] and c1[2] == c2[2])
+            or (abs(c1[2] - c2[2]) == 1 and c1[0] == c2[0] and c1[1] == c2[1])):
                 sides -= 1
     area += sides
 
