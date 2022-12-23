027b9deb560afd76dd78d9501e6efd6a11d0d56f
Sophie Chiang <sophieyoshi2014@gmail.com>
2022-12-23 09:17:43 +0100

    day22

---
+++ AoC 2022/day22.py
@@ -0,0 +1,53 @@
+with open("day22input.txt") as f:
+    file = f.read().splitlines()
+plane, ins, direction = file[:-2], file[-1], "RDLU"
+x, y, dire = plane[0].index("."), 0, direction[0]
+
+moves, temp = [], ""
+for i in ins:
+    if i == "L" or i == "R":
+        moves.append(temp)
+        moves.append(i)
+        temp = ""
+    else:
+        temp += i
+moves.append(temp)
+
+for i in moves:
+    if i == "L":
+        dire = direction[(direction.index(dire) - 1) % 4]
+    elif i == "R":
+        dire = direction[(direction.index(dire) + 1) % 4]
+    else:
+        lx, ly = len(plane[y].strip()), len(''.join([i[x] for i in plane if x < len(i)]).strip())
+        if dire == "R":
+            dx, dy = 1, 0
+        elif dire == "D":
+            dx, dy = 0, 1
+        elif dire == "L":
+            dx, dy = -1,0
+        else:
+            dx, dy = 0,-1
+
+        for _ in range(int(i)):
+            tempx = x + dx
+            if tempx >= len(plane[y]):
+                tempx -= lx
+            elif tempx < (len(plane[y])-lx):
+                tempx %= lx
+            tempy = y + dy
+
+            lengthy = len(''.join([i[x] for i in plane if x < len(i)]).rstrip())
+            if tempy >= lengthy:
+                tempy -= ly
+            elif tempy < (lengthy-ly):
+                tempy = lengthy - 1
+
+            if plane[tempy][tempx] == "#":
+                break
+            else:
+                x, y = tempx, tempy
+
+p = {"R": 0, "D": 1, "L":2, "U":3}
+password = 1000*(y+1)+4*(x+1)+p[dire]
+print(password)
